f6ed8f3de08a3b7fabed7fc904e4d32404e74126
cgrdmz <cgrdmzz@gmail.com>
2023-07-31 04:08:45 +0300

    multicore support for training

--- src/engine/cosine_similarity_engine.rs
+++ src/engine/cosine_similarity_engine.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, fs::File, io::BufWriter, path::Path, time::Instant};
+use std::{collections::HashMap, fs::File, io::BufWriter, path::Path, time::Instant, sync::mpsc::channel};
 
 use crate::{
     core::{
@@ -40,32 +40,52 @@ impl similarity::SimilarityEngine for CosineSimilarityEngineInMemory {
         // create item-user matrix from the events, for now we do not have different event types but we can have them in the future!
         let item_user_matrix = self.item_user_matrix();
 
+        let (tx, rx) = channel();
+
+        let pool = threadpool::ThreadPool::new(12);
+
         let start = Instant::now();
         // find similarities pair-wise
-        let mut similarities: DetailedRecommendations = HashMap::new();
         for (item_id, users) in item_user_matrix {
-            let a = similarities.entry(item_id.clone()).or_default();
-
+            // let a = similarities.entry(item_id.clone()).or_default();
             for (item_id_2, users_2) in item_user_matrix {
-                let c_sim = cosine_similarity(users, users_2);
-                if c_sim == 0f64 || item_id == item_id_2 {
-                    continue;
-                }
-                a.push((item_id_2.clone(), cosine_similarity(users, users_2)));
+                let tx = tx.clone();
+
+                let item_id_clone = item_id.clone();
+                let item_2_id_clone= item_id_2.clone();
+                let users_clone = users.clone();
+                let users_2_clone = users_2.clone();
+                pool.execute(move || {
+                    let c_sim = cosine_similarity(&users_clone, &users_2_clone);
+                    tx.send((item_id_clone, item_2_id_clone, c_sim)).unwrap();
+                });
+
+                // a.push((item_id_2.clone(), cosine_similarity(users, users_2)));
             }
-            a.sort_by(|(_, a), (_, b)| b.partial_cmp(a).unwrap())
+            // a.sort_by(|(_, a), (_, b)| b.partial_cmp(a).unwrap())
         }
         println!("took {:?} to train", start.elapsed());
+
+        let mut similarities: DetailedRecommendations = HashMap::new();
+        for (item_1, item_2, similarity) in rx.iter().take(item_user_matrix.len().pow(2)) {
+            if similarity == 0f64 || item_1 == item_2 {
+                return;
+            }
+
+            similarities.entry(item_1.clone()).or_default().push((item_2.clone(), similarity));
+        }
+
 
         // println!("{:?}", similarities)// [&"Lyft".to_string()][&"Uber".to_string()]);
         let path = Path::new("./data/output.json");
         let file = File::create(path).unwrap();
         let writer_buf = BufWriter::new(file);
         let filter_no_recs = similarities
-            .iter()
+            .iter_mut()
             .filter(|i| i.1.len() != 0)
             .map(|(product_id, recs)| {
-                (product_id.clone(), recs.iter().take(10).collect::<Vec<_>>())
+                recs.sort_by(|a, b| a.1.partial_cmp(&b.1).unwrap());
+                return (product_id.clone(), recs.iter().take(10).collect::<Vec<_>>().clone())
             })
             .collect::<HashMap<_, _>>();
 
